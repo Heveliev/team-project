3f69b911c7da371d91a5593fdc773e3e7d849c1d
Heveliev-destress <tolik05.03@ukr.net>
2023-04-01 14:46:16 +0300

    add hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+
 import { nanoid } from 'nanoid';
 
 import {
@@ -11,59 +11,46 @@ import {
   Text,
   Todo,
 } from 'components';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    todos: [],
-  };
 
-  componentDidMount() {
-    const todos = JSON.parse(localStorage.getItem('todos'));
 
-    if (todos) {
-      this.setState(() => ({ todos }));
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const { todos } = this.state;
+export const App = () => {
+  const [todos, setTodos] = useState(() => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+  })
 
-    if (prevState.todos !== todos) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
-  }
-
-  addTodo = text => {
-    const todo = {
-      id: nanoid(),
-      text,
+  
+  useEffect(() => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+    
+  }, [todos])
+  
+  const addTodo = text => {
+      const todo = {
+        id: nanoid(),
+        text,
     };
-
-    this.setState(({ todos }) => ({
-      todos: [...todos, todo],
-    }));
+        setTodos((prevState)=>[...prevState,todo]);
+  };
+  const   handleSubmit = data => {
+  addTodo(data);
+  };
+    const   deleteTodo = id => {
+    setTodos(prevState => (
+     prevState.filter(todo => todo.id !== id)
+    ));
   };
 
-  handleSubmit = data => {
-    this.addTodo(data);
-  };
-
-  deleteTodo = id => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== id),
-    }));
-  };
-
-  render() {
-    const { todos } = this.state;
-
-    return (
-      <>
+  return (
+    <>
         <Header />
-        <Section>
-          <Container>
-            <SearchForm onSubmit={this.handleSubmit} />
+         <Section>
+           <Container>
+             <SearchForm onSubmit={handleSubmit} />
 
-            {todos.length === 0 && (
+             {todos.length === 0 && (
               <Text textAlign="center">There are no any todos ... </Text>
             )}
 
@@ -75,14 +62,59 @@ export class App extends Component {
                       id={todo.id}
                       text={todo.text}
                       counter={index + 1}
-                      onClick={this.deleteTodo}
+                      onClick={deleteTodo}
                     />
                   </GridItem>
                 ))}
             </Grid>
           </Container>
         </Section>
-      </>
-    );
-  }
+    </>
+  )
 }
+
+// export class App extends Component {
+
+
+
+
+
+
+
+
+
+
+
+//   render() {
+//     const { todos } = this.state;
+
+//     return (
+//       <>
+//         <Header />
+//         <Section>
+//           <Container>
+//             <SearchForm onSubmit={this.handleSubmit} />
+
+//             {todos.length === 0 && (
+//               <Text textAlign="center">There are no any todos ... </Text>
+//             )}
+
+//             <Grid>
+//               {todos.length > 0 &&
+//                 todos.map((todo, index) => (
+//                   <GridItem key={todo.id}>
+//                     <Todo
+//                       id={todo.id}
+//                       text={todo.text}
+//                       counter={index + 1}
+//                       onClick={this.deleteTodo}
+//                     />
+//                   </GridItem>
+//                 ))}
+//             </Grid>
+//           </Container>
+//         </Section>
+//       </>
+//     );
+//   }
+// }
